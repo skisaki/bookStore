e5349b34676b8cbce2fd9746b90140f558d09c59
EC2 Default User <ec2-user@ip-172-31-13-210.ap-northeast-2.compute.internal>
2021-05-18 09:30:57 +0000

    init

--- src/build-services-jk.sh
+++ src/build-services-jk.sh
@@ -26,12 +26,12 @@ VERSION=$1
 PREFIX=$2
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-pushd "$SCRIPTDIR/productpage"
-  aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com
-  docker build -t jk-ecr-repository/productpage-v1 .
-  docker tag jk-ecr-repository/productpage-v1:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/productpage-v1:latest
-  docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/productpage-v1:latest
-popd
+# pushd "$SCRIPTDIR/productpage"
+#   aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com
+#   docker build -t jk-ecr-repository/productpage-v1 .
+#   docker tag jk-ecr-repository/productpage-v1:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/productpage-v1:latest
+#   docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/productpage-v1:latest
+# popd
 
 
 # pushd "$SCRIPTDIR/details"
@@ -42,21 +42,21 @@ popd
 # popd
 
 
-# pushd "$SCRIPTDIR/reviews"
-#   aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com
-#   docker run --rm -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
-#   pushd reviews-wlpcfg
-#     docker build -t jk-ecr-repository/reviews-v1 --build-arg service_version=v2 \
-#     --build-arg enable_ratings=true .
-#     docker tag jk-ecr-repository/reviews-v1:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v1:latest
-#     docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v1:latest
+pushd "$SCRIPTDIR/reviews"
+  aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com
+  docker run --rm -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
+  pushd reviews-wlpcfg
+    # docker build -t jk-ecr-repository/reviews-v1 --build-arg service_version=v2 \
+    # --build-arg enable_ratings=true .
+    # docker tag jk-ecr-repository/reviews-v1:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v1:latest
+    # docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v1:latest
     
-#     docker build -t jk-ecr-repository/reviews-v2  --build-arg service_version=v3 \
-#     --build-arg enable_ratings=true --build-arg star_color=red .
-#     docker tag jk-ecr-repository/reviews-v2:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v2:latest
-#     docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v2:latest
-#   popd
-# popd
+    docker build -t jk-ecr-repository/reviews-v2  --build-arg service_version=v3 \
+    --build-arg enable_ratings=true --build-arg star_color=red .
+    docker tag jk-ecr-repository/reviews-v2:latest 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v2:latest
+    docker push 424202621926.dkr.ecr.ap-northeast-2.amazonaws.com/jk-ecr-repository/reviews-v2:latest
+  popd
+popd
 
 
 # pushd "$SCRIPTDIR/ratings"
